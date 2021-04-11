a81b5aecdc33d278f0fb35e095a7e8a616872dd7
Alex Oshin <alexoshin@gatech.edu>
2021-04-11 14:02:39 -0400

    Add instructions and footer (#4)

--- js/ca_main.js
+++ js/ca_main.js
@@ -3,5 +3,11 @@ function onload() {
     document.body.style.opacity = 1;
     caCanvas = setup_ca_canvas(document.getElementById('ca-canvas'), 50, 50);
     caCanvas.start();
+    var instructions = document.getElementById('instructions');
+    instructions.innerText = '----- Key commands -----\n'
+        + 'r: reset simulation\n'
+        + 'c: clear grid\n'
+        + 'p: pause/unpause\n'
+        + 's: pause and step forward';
 }
 window.addEventListener('load', onload);
